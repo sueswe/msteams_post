d6c6a6a57be20b5e46c191591c240ea5bd7266e0
Werner Süß <nookie.suess@gmail.com>
2023-10-11 20:20:47 +0200

    Initialcommit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "json", "~> 2.6"
+
+# gem "teams_connector", "~> 0.1.5"
+
+gem "microsoft_teams_incoming_webhook_ruby", "~> 1.0"
+
+gem "colorize", "~> 1.1"
+gem "optparse", "~> 0.3.1"
+
+gem "yaml", "~> 0.2.1"
+
+gem "erb", "~> 4.0"
---
+++ teams_webconnector.rb
@@ -0,0 +1,108 @@
+#!/usr/bin/ruby
+
+# https://github.com/pedrofurtado/microsoft_teams_incoming_webhook_ruby
+
+require 'microsoft_teams_incoming_webhook_ruby'
+require 'colorize'
+require 'optparse'
+require 'yaml'
+require 'erb'
+
+$stdout.sync = true 
+$stdin.sync = true
+
+
+# config-file where to find the web-URL (secret)
+class Account
+  def self.configuration
+    YAML.safe_load(ERB.new(File.read('msteams_connector.yaml')).result)
+  end
+end
+
+
+class Createconfig
+  def self.sceleton
+    if File.exists?('msteams_connector.yaml')
+      puts "ERROR: configfile already exists."
+      exit 1
+    else
+      File.open("msteams_connector.yaml", "a") do |line|
+        line.puts "channelname:"
+        line.puts "  name: \"the name\""
+        line.puts "  secret_uri: \"channel-URI\""
+      end
+    end
+  end
+end
+
+
+myname = File.basename(__FILE__)
+options = {}
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: #{myname} [options]"
+
+  options[:newconfig] = nil
+  opts.on('-n','--newconfig','Write new config file template') do |w|
+    options[:newconfig] = true
+  end
+
+  options[:list] = nil
+  opts.on('-l','--list','list configured channels.') do
+    options[:list] = true
+  end
+
+  options[:channel] = nil
+  opts.on('-c','--channel c','Channel where to post to.') do |n|
+    options[:channel] = n
+  end
+
+  options[:message] = nil
+  opts.on('-m ', '--message m', 'Message text.') do |x|
+    options[:message] = x
+  end
+end
+optparse.parse!
+
+
+if options[:newconfig] == true
+  Createconfig.sceleton
+  puts "done."
+  exit 0
+end
+
+if options[:list] == true
+  puts "Channel-list:"
+  config_options = YAML.load_file("msteams_connector.yaml")
+  config_options.each do |key, value|
+    puts " -> #{key}"
+  end
+  exit 0
+end
+
+if options[:message].nil?
+  puts "ERROR: What messagetext should i send?".cyan
+  puts ""
+  puts optparse
+  exit 2
+end
+
+if options[:channel].nil?
+  puts "ERROR: need a target channel.".cyan
+  puts ""
+  puts optparse
+  exit 2
+else
+  chat = options[:channel]
+  puts "Posting goes to: #{chat}"
+end
+
+
+
+uri = Account.configuration[chat]['secret_uri']
+puts "Fireing up, this may take a few seconds ..."
+message = MicrosoftTeamsIncomingWebhookRuby::Message.new do |m|
+    m.url = uri
+    m.text = options[:message].to_s
+  end
+  
+puts "message.send"
